d57237be91440d281ecf2b2b696d5584d851ec76
Lyes-P <123511675+Lyes-P@users.noreply.github.com>
2025-04-07 01:18:07 +0200

    Correction main

--- app/main.py
+++ app/main.py
@@ -1,4 +1,9 @@
 import sys
+import os
+
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(parent_dir)
+
 from PyQt5.QtWidgets import QApplication
 
 from controller.RuleExtractionController import RuleExtractionController
